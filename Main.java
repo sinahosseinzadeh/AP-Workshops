31513972c2e532411d5ec8e1f41ee2cb1adc9baa
sinahosseinzadeh <58389375+sinahosseinzadeh@users.noreply.github.com>
2020-11-08 01:11:40 +0330

    aplab6

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
         Patient patient1 = new Patient("sina","2000","Male","Military","Bachelor","Tehran");
 //        Case sinaCase2 = new Case("Blood","NOTHING","Test","Test","Test","Test" , doctor1);
         Illness COVID19 = new Illness("COVID-19", "HIGH");
-        Case ahmadCase = new Case(COVID19,"He's dying","There is No Hope","Faith","2020",doctor1);
+        Case sinaCase = new Case(COVID19,"He's dying","There is No Hope","Faith","2020",doctor1);
 
 
 
